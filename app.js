bbab40dc3cf94062cdf82cbce41ec15cebf53eb2
manavvpopli-del <manavvpopli@gmail.com>
2026-01-11 21:39:44 +0400

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ function login() {
 
   localStorage.setItem("sessionKey", key);
   save();
-  location.href = "game.html";
+  location.href = "select.html";
 }
 
 /* ========== SESSION CHECK ========== */
